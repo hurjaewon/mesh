3116932c1906a1858a4e5d94164e6e955de5cabc
jwhur <jwhur@mwnl.snu.ac.kr>
2018-04-03 20:06:46 +0900

    180403 jwhur rssi_calibration update

--- ns-allinone-3.21/ns-3.21/rssi_cal/InputCalib.m
+++ ns-allinone-3.21/ns-3.21/rssi_cal/InputCalib.m
@@ -1,34 +1,41 @@
-function out = InputCalib(str)
-fileStr = ['measurements/' str '-log.dat'];
+function out = InputCalib(date, iter, mac6)
+fileStr = ['measurements/' date '-' iter '-' mac6 '.txt'];
 file_input = fopen(fileStr, 'r');
 
-input = fscanf(file_input, '%f');
+formatSpec = ['%f' '%f'];
+sizeInput = [7 Inf];
 
-LOC = zeros(length(input)/4, 2);
-RSS = zeros(length(input)/4, 1);
-AP = zeros(length(input)/4, 1);
+TIME = null(1);
+LOC = null(1, 2);
+RSS = null(1);
+MAC_OWN = null(1);
+AP = null(1);
 
-a = 1;
-b = 1;
-c = 1;
-d = 1;
-for i=1:length(input)
-	if mod(i, 4) == 1
-		LOC(a, 1) = input(i);
-		a = a + 1;
-	elseif mod(i, 4) == 2
-		LOC(b, 2) = input(i);
-		b = b + 1;
-	elseif mod(i, 4) == 3
-		RSS(c) = input(i);
-		c = c + 1;
-	else
-		AP(d) = input(i);
-		d = d + 1;
+while ~feof(file_input)
+	input = fscanf(file_input, '%f');
+	TIME = [TIME; input];
+	input = fscanf(file_input, '%[0123456789ABCDEF:]');
+	MAC_OWN = [MAC_OWN; input];
+	input = fscanf(file_input, '%f %f %f');
+	LOC = [LOC; input(2) input(3)];
+	input = fscanf(file_input, '%[0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_"()]');
+	AP = [AP; input];
+	input = fscanf(file_input, ['%f' '\n'], [1]);
+	RSS = [RSS; input];
+end
+fclose(file_input);
+
+AP_NUM = zeros(length(AP(:,1)), 1);
+
+for i = 1:length(AP(:, 1))
+	if AP(i, :) == '"MWNL_MSJANG(5G)"'
+		AP_NUM(i) = 15;
+	elseif AP(i, :) == '"MWNL_MAP1(5G)"'
+		AP_NUM(i) = 0;
+	elseif AP(i, :) == '"MWNL_MAP2(5G)"'
+		AP_NUM(i) = 1;
 	end
 end
 
-fclose(file_input);
-str = str(1:6);
-out = Calibration(LOC, RSS, AP, str);
+out = Calibration(LOC, RSS, AP_NUM, date);
 end
